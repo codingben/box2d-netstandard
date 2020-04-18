0a5ce6262d85617077675248a596b5c6efa10fd7
Ben Ukhanov <ben1zuk321@gmail.com>
2020-04-18 21:44:00 +0200

    :alien: Removed unused libraries.

--- src/box2dx/Box2D.NetStandard/Common/XForm.cs
+++ src/box2dx/Box2D.NetStandard/Common/XForm.cs
@@ -19,10 +19,6 @@
   3. This notice may not be removed or altered from any source distribution.
 */
 
-using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace Box2DX.Common
 {
 	/// <summary>
