790b416aacad3ae7400b74a4f19f6e04e352a895
Ben Ukhanov <ben1zuk321@gmail.com>
2020-04-25 22:29:36 +0200

    :alien: Code refactoring.

---
+++ examples/window/HelloWorld/Constants.cs
@@ -0,0 +1,9 @@
+namespace HelloWorld
+{
+    public static class Constants
+    {
+        public const float MinimumCameraZoom = 0.001f;
+        public const float MouseMoveSpeed = 0.001f;
+        public const float KeyboardMoveSpeed = 1f;
+    }
+}
