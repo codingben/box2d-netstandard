c63e62e1e4157a1cb2f0aa95bd6f2014c2340fd7
Ben Ukhanov <ben1zuk321@gmail.com>
2020-04-25 18:23:11 +0200

    :alien: Code refactoring.

--- examples/window/HelloWorld/Program.cs
+++ examples/window/HelloWorld/Program.cs
@@ -2,9 +2,9 @@
 
 namespace HelloWorld
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
             Console.WriteLine("Hello World!");
         }
