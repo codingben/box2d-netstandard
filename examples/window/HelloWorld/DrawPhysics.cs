767a11e4811fc35210930a60a63176d019ad288b
Ben Ukhanov <ben1zuk321@gmail.com>
2020-04-25 18:28:40 +0200

    :alien: Code refactoring.

--- examples/window/HelloWorld/DrawPhysics.cs
+++ examples/window/HelloWorld/DrawPhysics.cs
@@ -6,41 +6,41 @@ namespace HelloWorld
 {
     public class DrawPhysics : DebugDraw
     {
-        private readonly IPhysicsSimulationWindow physicsSimulationWindow;
+        private readonly IWindow window;
 
-        public DrawPhysics(IPhysicsSimulationWindow physicsSimulationWindow)
+        public DrawPhysics(IWindow window)
         {
-            this.physicsSimulationWindow = physicsSimulationWindow;
+            this.window = window;
         }
 
         public override void DrawPolygon(Vec2[] vertices, int vertexCount, Color color)
         {
-            physicsSimulationWindow.DrawPolygon(vertices, vertexCount, color);
+            window.DrawPolygon(vertices, vertexCount, color);
         }
 
         public override void DrawSolidPolygon(Vec2[] vertices, int vertexCount, Color color)
         {
-            physicsSimulationWindow.DrawSolidPolygon(vertices, vertexCount, color);
+            window.DrawSolidPolygon(vertices, vertexCount, color);
         }
 
         public override void DrawCircle(Vec2 center, float radius, Color color)
         {
-            physicsSimulationWindow.DrawCircle(center, radius, color);
+            window.DrawCircle(center, radius, color);
         }
 
         public override void DrawSolidCircle(Vec2 center, float radius, Vec2 axis, Color color)
         {
-            physicsSimulationWindow.DrawSolidCircle(center, radius, axis, color);
+            window.DrawSolidCircle(center, radius, axis, color);
         }
 
         public override void DrawSegment(Vec2 p1, Vec2 p2, Color color)
         {
-            physicsSimulationWindow.DrawSegment(p1, p2, color);
+            window.DrawSegment(p1, p2, color);
         }
 
         public override void DrawXForm(XForm xf)
         {
-            physicsSimulationWindow.DrawXForm(xf);
+            window.DrawXForm(xf);
         }
     }
 }
