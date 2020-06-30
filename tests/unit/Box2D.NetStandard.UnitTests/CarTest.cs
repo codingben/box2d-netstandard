93a8c3181712cf9e944bfe16463ddc49a843acfe
Ben Ukhanov <ben1zuk321@gmail.com>
2020-06-30 23:36:57 +0300

    :alien: Improved readability of the code.

--- tests/unit/Box2D.NetStandard.UnitTests/CarTest.cs
+++ tests/unit/Box2D.NetStandard.UnitTests/CarTest.cs
@@ -5,28 +5,33 @@ using TestWorlds;
 using Xunit;
 using Xunit.Abstractions;
 
-namespace Box2D.NetStandard.UnitTests {
-  public class CarTest {
-    private ITestOutputHelper output;
+namespace Box2D.NetStandard.UnitTests
+{
+    public class CarTest
+    {
+        private ITestOutputHelper output;
 
-    public CarTest(ITestOutputHelper output) {
-      this.output = output;
+        public CarTest(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        [Fact]
+        public void CarTest1()
+        {
+            World world = Car.CreateWorld(out Body[] bodies, out Joint[] joints);
+
+            for (int i = 0; i < 1400; i++)
+                world.Step(0.016f, 8, 3);
+
+            Assert.True(bodies[1].GetPosition().X > 275f);
+            Assert.True(bodies[2].GetPosition().X > 225f);
+            Assert.True(bodies[3].GetPosition().X > 225f);
+            Assert.True(bodies[4].GetPosition().X > 225f);
+            Assert.True(bodies[5].GetPosition().X > 225f);
+            Assert.True(bodies[6].GetPosition().X > 225f);
+            Assert.True(bodies[7].GetPosition().X > 275f);
+            Assert.True(bodies[8].GetPosition().X > 275f);
+        }
     }
-
-    [Fact]
-    public void CarTest1() {
-      World world = Car.CreateWorld(out Body[] bodies, out Joint[] joints);
-      for (int i = 0; i < 1400; i++)
-        world.Step(0.016f, 8, 3);
-
-      Assert.True(bodies[1].GetPosition().X > 275f);
-      Assert.True(bodies[2].GetPosition().X > 225f);
-      Assert.True(bodies[3].GetPosition().X > 225f);
-      Assert.True(bodies[4].GetPosition().X > 225f);
-      Assert.True(bodies[5].GetPosition().X > 225f);
-      Assert.True(bodies[6].GetPosition().X > 225f);
-      Assert.True(bodies[7].GetPosition().X > 275f);
-      Assert.True(bodies[8].GetPosition().X > 275f);
-    }
-  }
 }
