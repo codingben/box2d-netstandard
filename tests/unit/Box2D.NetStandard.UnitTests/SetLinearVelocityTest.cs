97fa5e44b346d1cc6f8879e655e748fa883fae0e
Ben Ukhanov <ben1zuk321@gmail.com>
2020-06-30 23:41:39 +0300

    :alien: Improved readability of the code.

--- tests/unit/Box2D.NetStandard.UnitTests/SetLinearVelocityTest.cs
+++ tests/unit/Box2D.NetStandard.UnitTests/SetLinearVelocityTest.cs
@@ -6,48 +6,66 @@ using Microsoft.VisualStudio.TestPlatform.Utilities;
 using Xunit;
 using Xunit.Abstractions;
 
-namespace Box2D.NetStandard.UnitTests {
-  public class SetLinearVelocityTest {
-    private readonly ITestOutputHelper _output;
-    public SetLinearVelocityTest(ITestOutputHelper output) {
-      _output = output;
+namespace Box2D.NetStandard.UnitTests
+{
+    public class SetLinearVelocityTest
+    {
+        private readonly ITestOutputHelper _output;
+
+        public SetLinearVelocityTest(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void TestOnlyOneBodyGetsVelocity()
+        {
+            World world = new World();
+
+            Body ground = world.CreateBody(
+              new BodyDef
+              {
+                  position = new Vector2(0, -1)
+              });
+            ground.CreateFixture(new EdgeShape(new Vector2(-100, 0), new Vector2(100, 0)));
+
+            Body dynamic1 = world.CreateBody(
+              new BodyDef
+              {
+                  position = new Vector2(-10, 3),
+                  type = BodyType.Dynamic
+              });
+            dynamic1.CreateFixture(new PolygonShape(2, 2), 1f);
+
+            Body dynamic2 = world.CreateBody(
+              new BodyDef
+              {
+                  position = new Vector2(10, 3),
+                  type = BodyType.Dynamic
+              });
+            dynamic2.CreateFixture(new PolygonShape(2, 2), 1f);
+
+            // Let them fall to the floor
+            world.Step(1000, 10, 10);
+
+            Vector2 starting1 = dynamic1.GetPosition();
+            Vector2 starting2 = dynamic2.GetPosition();
+
+            // Apply force to one object so it should move away from the other
+            dynamic1.SetLinearVelocity(new Vector2(-10, 0));
+
+            world.Step(100, 10, 10);
+
+            Vector2 ending1 = dynamic1.GetPosition();
+            Vector2 ending2 = dynamic2.GetPosition();
+
+            // It should not have moved vertically
+            Assert.True((starting1.Y - ending1.Y) < 0.01f);
+            // It should have moved left
+            Assert.True(ending1.X < starting1.X);
+
+            // Body 2 should not have moved
+            Assert.Equal(starting2, ending2);
+        }
     }
-
-    [Fact]
-    public void TestOnlyOneBodyGetsVelocity() {
-      World world = new World();
-      
-      Body ground = world.CreateBody(new BodyDef {position = new Vector2(0, -1)});
-      ground.CreateFixture(new EdgeShape(new Vector2(-100,0), new Vector2(100,0)) );
-
-      Body dynamic1 = world.CreateBody(new BodyDef {position = new Vector2(-10, 3), type = BodyType.Dynamic});
-      dynamic1.CreateFixture(new PolygonShape(2, 2), 1f);
-      
-      Body dynamic2 = world.CreateBody(new BodyDef {position = new Vector2(10, 3), type = BodyType.Dynamic});
-      dynamic2.CreateFixture(new PolygonShape(2, 2), 1f);
-
-      // Let them fall to the floor
-      world.Step(1000,10,10);
-
-      Vector2 starting1 = dynamic1.GetPosition();
-      Vector2 starting2 = dynamic2.GetPosition();
-      
-      // Apply force to one object so it should move away from the other
-      dynamic1.SetLinearVelocity(new Vector2(-10,0) );
-
-      world.Step(100, 10, 10);
-
-      Vector2 ending1 = dynamic1.GetPosition();
-      Vector2 ending2 = dynamic2.GetPosition();
-      
-      // It should not have moved vertically
-      Assert.True((starting1.Y-ending1.Y)<0.01f);
-      // It should have moved left
-      Assert.True(ending1.X < starting1.X);
-
-      // Body 2 should not have moved
-      Assert.Equal(starting2, ending2);
-
-    }
-  }
 }
