3d539377de4df7d2515e30bc2b6625ed33de96aa
Ben Ukhanov <ben1zuk321@gmail.com>
2020-08-19 11:58:44 +0300

    :alien: Mouse: Improved code readability.

--- src/box2dx/Box2D.NetStandard/Dynamics/Joints/Mouse/MouseJointDef.cs
+++ src/box2dx/Box2D.NetStandard/Dynamics/Joints/Mouse/MouseJointDef.cs
@@ -1,39 +1,40 @@
 using System.Numerics;
 
-namespace Box2D.NetStandard.Dynamics.Joints.Mouse {
-  /// <summary>
-  /// Mouse joint definition. This requires a world target point,
-  /// tuning parameters, and the time step.
-  /// </summary>
-  public class MouseJointDef : JointDef
-  {
-    public MouseJointDef()
+namespace Box2D.NetStandard.Dynamics.Joints.Mouse
+{
+    /// <summary>
+    /// Mouse joint definition. This requires a world target point,
+    /// tuning parameters, and the time step.
+    /// </summary>
+    public class MouseJointDef : JointDef
     {
-      FrequencyHz  = 5.0f;
-      DampingRatio = 0.7f;
+        public MouseJointDef()
+        {
+            FrequencyHz = 5.0f;
+            DampingRatio = 0.7f;
+        }
+
+        /// <summary>
+        /// The initial world target point. This is assumed
+        /// to coincide with the body anchor initially.
+        /// </summary>
+        public Vector2 Target;
+
+        /// <summary>
+        /// The maximum constraint force that can be exerted
+        /// to move the candidate body. Usually you will express
+        /// as some multiple of the weight (multiplier * mass * gravity).
+        /// </summary>
+        public float MaxForce;
+
+        /// <summary>
+        /// The response speed.
+        /// </summary>
+        public float FrequencyHz;
+
+        /// <summary>
+        /// The damping ratio. 0 = no damping, 1 = critical damping.
+        /// </summary>
+        public float DampingRatio;
     }
-
-    /// <summary>
-    /// The initial world target point. This is assumed
-    /// to coincide with the body anchor initially.
-    /// </summary>
-    public Vector2 Target;
-
-    /// <summary>
-    /// The maximum constraint force that can be exerted
-    /// to move the candidate body. Usually you will express
-    /// as some multiple of the weight (multiplier * mass * gravity).
-    /// </summary>
-    public float MaxForce;
-
-    /// <summary>
-    /// The response speed.
-    /// </summary>
-    public float FrequencyHz;
-
-    /// <summary>
-    /// The damping ratio. 0 = no damping, 1 = critical damping.
-    /// </summary>
-    public float DampingRatio;
-  }
 }
