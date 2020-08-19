2d75541de1d5cacbd49b5f91fb2ee5632ae40c0d
Ben Ukhanov <ben1zuk321@gmail.com>
2020-08-19 12:02:03 +0300

    :alien: Weld: Improved code readability.

--- src/box2dx/Box2D.NetStandard/Dynamics/Joints/Weld/WeldJointDef.cs
+++ src/box2dx/Box2D.NetStandard/Dynamics/Joints/Weld/WeldJointDef.cs
@@ -1,25 +1,29 @@
 using System;
 using System.Numerics;
 
-namespace Box2D.NetStandard.Dynamics.Joints.Weld {
-  public class WeldJointDef : JointDef {
+namespace Box2D.NetStandard.Dynamics.Joints.Weld
+{
+    public class WeldJointDef : JointDef
+    {
+        [Obsolete("Use Joint.AngularStiffness to get stiffness & damping values", true)]
+        public float frequencyHz;
 
-    [Obsolete("Use Joint.AngularStiffness to get stiffness & damping values",true)]
-    public float frequencyHz;
-    [Obsolete("Use Joint.AngularStiffness to get stiffness & damping values",true)]
-    public float dampingRatio;
-    
-    public Vector2 localAnchorA;
-    public Vector2 localAnchorB;
-    public float referenceAngle;
-    /// <summary>
-    /// The rotational stiffness in N*m
-    /// Disable softness with a value of 0
-    /// </summary>
-    public float stiffness;
-    /// <summary>
-    /// The rotational damping in N*m*s
-    /// </summary>
-    public float damping;
-  }
+        [Obsolete("Use Joint.AngularStiffness to get stiffness & damping values", true)]
+        public float dampingRatio;
+
+        public Vector2 localAnchorA;
+        public Vector2 localAnchorB;
+        public float referenceAngle;
+
+        /// <summary>
+        /// The rotational stiffness in N*m
+        /// Disable softness with a value of 0
+        /// </summary>
+        public float stiffness;
+
+        /// <summary>
+        /// The rotational damping in N*m*s
+        /// </summary>
+        public float damping;
+    }
 }
