f6be0f6d06773d55139d1a3eec2fae3cd55a7a3a
Ben Ukhanov <ben1zuk321@gmail.com>
2020-08-19 12:04:43 +0300

    :alien: Joints: Improved code readability.

--- src/box2dx/Box2D.NetStandard/Dynamics/Joints/IMotorisedJoint.cs
+++ src/box2dx/Box2D.NetStandard/Dynamics/Joints/IMotorisedJoint.cs
@@ -2,16 +2,20 @@ using System.Runtime.CompilerServices;
 
 namespace Box2D.NetStandard.Dynamics.Joints
 {
-  public interface IMotorisedJoint {
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public void SetMotorSpeed(float speed);
+    public interface IMotorisedJoint
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void SetMotorSpeed(float speed);
 
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public float GetMotorSpeed();
-		
-    public float MotorSpeed {
-      [MethodImpl(MethodImplOptions.AggressiveInlining)]get;
-      [MethodImpl(MethodImplOptions.AggressiveInlining)]set;
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public float GetMotorSpeed();
+
+        public float MotorSpeed
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get;
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            set;
+        }
     }
-  }
 }
