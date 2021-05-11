a15d1424de43bd817a6b958e6bb0dc38d52f6c2d
Ben Ukhanov <ben1zuk321@gmail.com>
2021-05-11 12:19:34 +0300

    Update min camera zoom

--- examples/Box2D.Window/WindowSettings.cs
+++ examples/Box2D.Window/WindowSettings.cs
@@ -1,12 +1,12 @@
 ﻿/*
-    Window Simulation Copyright © Ben Ukhanov 2020
-*/
+ * Window Simulation Copyright © Ben Ukhanov 2021
+ */
 
 namespace Box2D.Window
 {
     public static class WindowSettings
     {
-        public const float MinimumCameraZoom = 0.001f;
+        public const float MinimumCameraZoom = 0.05f;
         public const float MouseMoveSpeed = 0.001f;
         public const float KeyboardMoveSpeed = 1f;
     }
