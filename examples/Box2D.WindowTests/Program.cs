76177332c06aa92451395ed2f0bf6909cca4f823
Ben Ukhanov <ben1zuk321@gmail.com>
2021-05-28 18:36:37 +0300

    Removed unused methods

--- examples/Box2D.WindowTests/Program.cs
+++ examples/Box2D.WindowTests/Program.cs
@@ -28,8 +28,6 @@ namespace Box2D.WindowTests
 
         private static void CreateWorld()
         {
-            // world = CreateWorld();
-
             world = RubeGoldbergWorld.CreateWorld(out Body[] bodies, out Joint[] joints);
 
             // world = AddPairWorld.CreateWorld();
@@ -54,18 +52,12 @@ namespace Box2D.WindowTests
                 game.SetView(new CameraView(position: new Vector2(12, 5)));
 
                 var physicsDrawer = new DrawPhysics(game);
-
                 // physicsDrawer.AppendFlags(DrawFlags.Aabb);
-
                 physicsDrawer.AppendFlags(DrawFlags.Shape);
-
                 // physicsDrawer.AppendFlags(DrawFlags.Pair);
-
                 physicsDrawer.AppendFlags(DrawFlags.Joint);
 
                 world.SetDebugDraw(physicsDrawer);
-
-                // CreateBodies();
 
                 game.VSync = OpenTK.VSyncMode.Off;
                 game.Run(60.0, 60.0);
@@ -98,63 +90,6 @@ namespace Box2D.WindowTests
         private static void OnDisposed(object sender, EventArgs eventArgs)
         {
             world?.SetDebugDraw(null);
-            // world?.Dispose();
         }
-
-        #region Deprecated
-        /*private static void CreateBodies()
-        {
-            AddBox(new Vec2(0.0f, 10.0f), new Vec2(5.0f, 5.0f));
-            AddBox(new Vec2(0.0f, 20.0f), new Vec2(5.0f, 5.0f));
-
-            var box = AddBox(new Vec2(2.5f, 25.0f), new Vec2(5.0f, 5.0f));
-            box.SetLinearVelocity(new Vec2(5, 0));
-            box.SetAngularVelocity(5);
-
-            AddBox(new Vec2(-2.5f, 25.0f), new Vec2(5.0f, 5.0f));
-            AddStaticBox(new Vec2(0.0f, -10.0f), new Vec2(50.0f, 10.0f));
-        }
-
-        private static Body AddBox(Vec2 position, Vec2 size)
-        {
-            var bodyDefinition = new BodyDef();
-            bodyDefinition.position.Set(position.X, position.Y);
-
-            var boxDefinition = new PolygonDef();
-            boxDefinition.SetAsBox(size.X, size.Y);
-            boxDefinition.Density = 1.0f;
-            boxDefinition.Friction = 0.3f;
-            boxDefinition.Restitution = 0.2f;
-
-            var body = world?.CreateBody(bodyDefinition);
-            body.CreateFixture(boxDefinition);
-            body.SetMassFromShapes();
-
-            return body;
-        }
-
-        private static void AddStaticBox(Vec2 position, Vec2 size)
-        {
-            var bodyDefinition = new BodyDef();
-            bodyDefinition.Position.Set(position.X, position.Y);
-
-            var boxDefinition = new PolygonDef();
-            boxDefinition.SetAsBox(size.X, size.Y);
-            boxDefinition.Density = 0.0f;
-
-            var body = world?.CreateBody(bodyDefinition);
-            body.CreateFixture(boxDefinition);
-            body.SetMassFromShapes();
-        }
-
-        private static World CreateWorld()
-        {
-            var aABB = new AABB();
-            aABB.LowerBound.Set(-100.0f, -100.0f);
-            aABB.UpperBound.Set(100.0f, 100.0f);
-
-            return new World(aABB, gravity: new Vec2(0.0f, -9.8f), doSleep: true);
-        }*/
-        #endregion
     }
 }
